8531349d31e4d6327317f5c2b99e3f8d5261f9cd
Maha <maha@recruitly.io>
2024-11-26 11:02:20 +0530

    card changes

--- src/components/movieList/MovieCard.jsx
+++ src/components/movieList/MovieCard.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import "./MovieCard.css";
 import Star from "../../assets/star.png";
 
-const movieCard = (movie) => {
+const movieCard = ({ movie }) => {
   return (
     <a href="" className="movie_card">
-      console.log(movie.Title);
       <img src={movie.Poster} alt="movie poster" className="movie_poster" />
       <div className="movie_details">
         <h3 className="movie_details_heading">{movie.Title}</h3>
         <div className=" align_center movie_date_rate">
-          <p>{movie.year}</p>
+          <p>{movie.Year}</p>
           <p>
-            {movie.rate_average}
+            {movie.Type}
             <img src={Star} alt="rating icon" className="card_emoji" />
           </p>
         </div>
-        <p className="movie_description">{movie.type}</p>
+        <p className="movie_description">{movie.Type}</p>
       </div>
     </a>
   );
