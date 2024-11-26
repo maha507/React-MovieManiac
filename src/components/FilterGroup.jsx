4adb3bc5ab7444026d244cd77d29ae80baaf0579
Maha <maha@recruitly.io>
2024-11-26 17:59:13 +0530

    ui changes

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -2,23 +2,17 @@ import React from "react";
 
 const FilterGroup = ({ minRating, onRatingClick, ratings }) => {
   return (
-    <div>
-      <ul className="align_center movie_filter">
-        {ratings.map((rate) => (
-          <li
-            className={
-              minRating === rate
-                ? "movie_filter_item active"
-                : "movie_filter_item"
-            }
-            key={rate}
-            onClick={() => onRatingClick({ rate })}
-          >
-            {rate}+ Star
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="align_center movie_filter">
+      {ratings.map((rate) => (
+        <li
+          className={`movie_filter_item ${minRating === rate ? "active" : ""}`}
+          key={rate}
+          onClick={() => onRatingClick(rate)}
+        >
+          {rate}+ star
+        </li>
+      ))}
+    </ul>
   );
 };
 
